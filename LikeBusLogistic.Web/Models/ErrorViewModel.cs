d954159000d506ca48dddda40d7e499584215fb3
Nikita Dermenzhi <nikita.dermenzi@gmail.com>
2019-11-01 11:08:58 -0400

    Init

---
+++ LikeBusLogistic.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LikeBusLogistic.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
