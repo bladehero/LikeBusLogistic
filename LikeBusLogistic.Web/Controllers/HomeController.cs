ad1b7b7aef539cb69c9d5b49cf51103921c872e4
bladehero <nikita.dermenzhi@geekgroupllc.com>
2019-11-19 03:08:19 +0200

    Update HomeController.cs

--- LikeBusLogistic.Web/Controllers/HomeController.cs
+++ LikeBusLogistic.Web/Controllers/HomeController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LikeBusLogistic.BLL;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LikeBusLogistic.Web.Controllers
 {
+    [Authorize]
     public class HomeController : BaseController
     {
         public HomeController(ServiceFactory serviceFactory) : base(serviceFactory)
