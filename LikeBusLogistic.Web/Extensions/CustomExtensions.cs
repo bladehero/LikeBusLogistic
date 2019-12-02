ea89dc1f2da2b4b6c1f95698e782887f83168742
Nikita Dermenzhi <nikita.dermenzi@gmail.com>
2019-12-02 11:39:19 -0500

    Fix

--- LikeBusLogistic.Web/Extensions/CustomExtensions.cs
+++ LikeBusLogistic.Web/Extensions/CustomExtensions.cs
@@ -18,38 +18,26 @@ namespace LikeBusLogistic.Web.Extensions
             if (attributes != null && attributes.Length > 0) return attributes[0].Description;
             else return source.ToString();
         }
-        public static HtmlString SelectList<T>(this IEnumerable<T> source, Func<T, object> value, Func<T, object> display, object selected = null, object attributes = null)
+
+        public static HtmlString SelectList<T>(this IEnumerable<T> source, Func<T, object> value, Func<T, object> display, object selected = null, object attributes = null, Func<T, object> optionAttributes = null)
         {
             var sb = new StringBuilder("<select");
             foreach (var property in attributes?.GetType().GetProperties())
             {
                 sb.Append(' ');
-                sb.Append(property.Name.ToLower());
+                sb.Append(property.Name.ToLower().Replace('_', '-'));
                 sb.Append("=\"");
                 sb.Append(property.GetValue(attributes));
                 sb.Append('\"');
             }
             sb.Append('>');
 
-            foreach (var option in source)
-            {
-                var optionValue = value(option);
-                sb.Append("<option value=\"");
-                sb.Append(optionValue);
-                sb.Append('\"');
-                if (optionValue.Equals(selected))
-                {
-                    sb.Append(" selected");
-                }
-                sb.Append('>');
-                sb.Append(display(option));
-                sb.Append("</option>");
-            }
+            _optionBuildHelper(sb, source, value, display, selected, optionAttributes);
 
             sb.Append("</select>");
             return new HtmlString(sb.ToString());
         }
-        public static HtmlString SelectListWithEmptyOption<T>(this IEnumerable<T> source, Func<T, object> value, Func<T, object> display, string emptyOption, object selected = null, object attributes = null)
+        public static HtmlString SelectListWithEmptyOption<T>(this IEnumerable<T> source, Func<T, object> value, Func<T, object> display, string emptyOption, object selected = null, object attributes = null, Func<T, object> optionAttributes = null)
         {
             var sb = new StringBuilder("<select");
             foreach (var property in attributes?.GetType().GetProperties())
@@ -66,12 +54,31 @@ namespace LikeBusLogistic.Web.Extensions
             sb.Append(emptyOption);
             sb.Append("</option>");
 
+            _optionBuildHelper(sb, source, value, display, selected, optionAttributes);
+
+            sb.Append("</select>");
+            return new HtmlString(sb.ToString());
+        }
+        private static void _optionBuildHelper<T>(StringBuilder sb, IEnumerable<T> source, Func<T, object> value, Func<T, object> display, object selected, Func<T, object> optionAttributes)
+        {
             foreach (var option in source)
             {
                 var optionValue = value(option);
                 sb.Append("<option value=\"");
                 sb.Append(optionValue);
                 sb.Append('\"');
+                if (optionAttributes != null)
+                {
+                    var attributes = optionAttributes(option);
+                    foreach (var attribute in attributes.GetType().GetProperties())
+                    {
+                        sb.Append(' ');
+                        sb.Append(attribute.Name.ToLower().Replace('_', '-'));
+                        sb.Append("=\"");
+                        sb.Append(attribute.GetValue(attributes));
+                        sb.Append('\"');
+                    }
+                }
                 if (optionValue.Equals(selected))
                 {
                     sb.Append(" selected");
@@ -80,10 +87,8 @@ namespace LikeBusLogistic.Web.Extensions
                 sb.Append(display(option));
                 sb.Append("</option>");
             }
-
-            sb.Append("</select>");
-            return new HtmlString(sb.ToString());
         }
+
         public static HtmlString HtmlEditIcon(string @class = "") => new HtmlString($"<span class=\"uk-text-warning {@class}\" uk-icon=\"cog\"></span>");
         public static HtmlString HtmlDeleteIcon(string @class = "") => new HtmlString($"<span class=\"uk-text-danger {@class}\" uk-icon=\"trash\"></span>");
         public static HtmlString HtmlRestoreIcon(string @class = "") => new HtmlString($"<span class=\"uk-text-success {@class}\" uk-icon=\"refresh\"></span>");
