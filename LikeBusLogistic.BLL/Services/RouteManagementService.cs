09614418e34138dfeddbc420c244a381880937de
Nikita Dermenzhi <nikita.dermenzi@gmail.com>
2019-12-03 15:54:50 -0500

    Init Route

---
+++ LikeBusLogistic.BLL/Services/RouteManagementService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LikeBusLogistic.BLL.Services
+{
+    public class RouteManagementService : BaseService
+    {
+        public RouteManagementService(string connection) : base(connection) { }
+
+        
+    }
+}
